79ed7f63901d05d8f5d7489d71d54a071522cf7a
Todd Croak-Falen <toddcf@gmail.com>
2017-06-30 20:50:57 -0700

    Made placeholder sections and footer.

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+$( document ).ready( function() {
+
+	// FOOTER
+
+	// Pass current year into footer copyright:
+	var thisYear = new Date().getFullYear();
+
+	if ( thisYear > 2017 ) {
+		// Footer (IIFE):
+		var footerInfo = ( function() {
+			$( ".currentYear" ).html( " &ndash; " + thisYear + " " );
+		}) ();
+	}
+
+} );
